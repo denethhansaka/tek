b04ebfb5c8621733cc0a5e0df4d4c8a9fd9db3f5
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-14 22:28:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "UzNT0LCY#HuqdNYQdQsf4DFJDl-rYKBBao-St8tXQgO0LsFr8l90",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "Put your alive messege",
 MENU_IMG : process.env.MENU_IMG || "https://telegra.ph/file/ef48cc8d2d26081f7d46a.jpg",
